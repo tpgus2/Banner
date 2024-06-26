98bf3c557d8120638242d55ca972519113b5c8ad
tpgus2 <gksmfdnjs02@gmail.com>
2024-06-26 11:56:30 +0900

    백그라운드로 이미지 처리해서 setinterval

--- setinterval.js
+++ setinterval.js
@@ -8,10 +8,36 @@
 // }, 1000)
 
 // **자바스크립트는 css와 다르다 연결된 html기준으로 처리해야한다.
-let count = 3;
+// let count = 3;
 
-setInterval(function(){
-    count++;
-    count %= 5;
-    document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`)
+// setInterval(function(){
+//     count++;
+//     count %= 5;
+//     document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`);
+// },3000)
+
+const bannerdata = [
+    "/banner2/membership.png",
+    "/banner/0.png",
+    "/banner2/poketmon.png",
+    "/banner/1.png"
+];
+
+let num = 0;
+document.querySelector("#bannerimg").setAttribute("src",bannerdata[num]);
+document.querySelector(".bannerwrap").style.background = `url(${bannerdata[num]}) center no-repeat`
+
+const autoBanner = setInterval(function(){
+
+    num++;
+    num %= bannerdata.length;
+    document.querySelector("#bannerimg").setAttribute("src",bannerdata[num]);
+    document.querySelector(".bannerwrap").style.background = `url(${bannerdata[num]}) center no-repeat`;
+
 },3000)
+
+
+document.querySelector(".bannerwrap button").addEventListener('click', function(){
+    clearInterval(autoBanner);
+    this.innerHTML = "play";
+})
