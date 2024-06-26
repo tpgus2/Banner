ef39fdfa5f3bf0005b7cdfac796135faeed4a964
tpgus2 <gksmfdnjs02@gmail.com>
2024-06-26 10:36:48 +0900

    setinterval 기초

---
+++ setinterval.js
@@ -0,0 +1,8 @@
+// let count = 0;
+// const navidata = ["네이버","다음","구글","아마존","깃","카카오"];
+
+// setInterval(function() {
+//     console.log(navidata[count]);
+//     count++;
+//     count %= navidata.length;
+// }, 1000)
