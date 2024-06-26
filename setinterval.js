d830476622fb2dcf1ccc8a2766e67708722e1dc5
tpgus2 <gksmfdnjs02@gmail.com>
2024-06-26 10:53:27 +0900

    img setinterval로 반복

--- setinterval.js
+++ setinterval.js
@@ -6,3 +6,12 @@
 //     count++;
 //     count %= navidata.length;
 // }, 1000)
+
+// **자바스크립트는 css와 다르다 연결된 html기준으로 처리해야한다.
+let count = 3;
+
+setInterval(function(){
+    count++;
+    count %= 5;
+    document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`)
+},3000)
